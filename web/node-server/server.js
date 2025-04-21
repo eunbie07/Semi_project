c1fd9937dedc9e0f9bd09dc6110a346ee16acc16
eunbie07 <eunbie07@gmail.com>
2025-04-21 19:53:07 +0900

    server

--- web/node-server/server.js
+++ web/node-server/server.js
@@ -21,6 +21,36 @@ app.get('/api/depression', async (req, res) => {
   }
 });
 
+app.get('/api/smartphone_badeffect', async (req, res) => {
+  const { age, gender, year } = req.query;
+
+  try {
+    const response = await axios.get('http://192.168.1.53:3001/smartphone_badeffect', {
+      params: { age, gender, year }
+    });
+    res.send(response.data); // 프론트엔드에 그대로 전달
+  } catch (error) {
+    console.error('백엔드 호출 오류:', error.message);
+    res.status(500).send({ result: false, message: '백엔드 오류 발생' });
+  }
+});
+
+app.get('/api/stress_top_issue', async (req, res) => {
+  const { year } = req.query;
+
+  try {
+    const response = await axios.get('http://192.168.1.23:3001/stress_top_issue', {
+      params: { year }
+    });
+    res.send(response.data); // 프론트엔드에 그대로 전달
+  } catch (error) {
+    console.error('백엔드 호출 오류:', error.message);
+    res.status(500).send({ result: false, message: '백엔드 오류 발생' });
+  }
+});
+
+
+
 // 3. 서버 실행
 app.listen(8000, function() {
   console.log("8000 port : Server Started~!!");
