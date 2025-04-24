0531b4ba4cec6cadb12146d14cdc4a75c89e27bc
2hanse <prsuw113@naver.com>
2025-04-24 11:07:02 +0900

    .

--- web_hs/public/quiz_loader.js
+++ web_hs/public/quiz_loader.js
@@ -1,20 +1,19 @@
 document.addEventListener("DOMContentLoaded", () => {
-    fetch("quiz_menu.html")
-      .then(res => res.text())
-      .then(html => {
-        document.getElementById("quiz").innerHTML = html;
+  fetch("quiz_menu.html")
+    .then(res => res.text())
+    .then(html => {
+      document.getElementById("quiz").innerHTML = html;
+    });
+});
+
+function loadQuiz(page) {
+  fetch(page)
+    .then(res => res.text())
+    .then(html => {
+      document.getElementById("quiz").innerHTML = html;
+      window.scrollTo({
+        top: document.getElementById("quiz").offsetTop,
+        behavior: "smooth"
       });
-  });
-  
-  function loadQuiz(page) {
-    fetch(page)
-      .then(res => res.text())
-      .then(html => {
-        document.getElementById("quiz").innerHTML = html;
-        window.scrollTo({
-          top: document.getElementById("quiz").offsetTop,
-          behavior: "smooth"
-        });
-      });
-  }
-  
+    });
+}
